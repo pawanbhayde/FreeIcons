02357cca836a31dbb34e27c7a6fa85dcb551b5e8
Pawan Bhayde <82137686+pawanbhayde@users.noreply.github.com>
2021-06-27 08:44:05 +0530

    Add files via upload

---
+++ Js/search.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".default_option").click(function () {
+    $(".dropdown ul").addClass("active");
+  });
+
+  $(".dropdown ul li").click(function () {
+    var text = $(this).text();
+    $(".default_option").text(text);
+    $(".dropdown ul").removeClass("active");
+  });
+});
